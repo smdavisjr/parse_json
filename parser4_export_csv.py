9a28554bd559a076ad8a5cf60629ea6508a49fbd
Stephen Davis <davisjr719@gmail.com>
2018-10-16 15:20:53 -0500

    paser4, parser4_export_csv, parser5

--- parser4_export_csv.py
+++ parser4_export_csv.py
@@ -1,11 +1,10 @@
 import json
 import csv
 # import sys
-# from pprint import pprint
+## from pprint import pprint
 
 with open('production.json') as file:
     data = json.load(file)
-
 json_str = json.dumps(data)
 resp = json.loads(json_str)
 
@@ -19,56 +18,3 @@ for key in resp:
     status = str(resp[region]['general']['active'])
     row = region + "," + status + "\n"
     csv.write(row)
-
-# #
-# #
-# # GET ALL REGION CODES AND ACTIVE STATUS
-# print('region code, is active')
-# print('-----')
-# for key in resp:
-#     region = key
-#     status = (resp[region]['general']['active'])
-#     print (region, status)
-# print('-----')
-# # print('')
-# #
-# #
-# # CLASSIFICATION OF REGIONS AS ACTIVE OR NON-ACTIVE
-# for key in resp:
-#     region = key
-#     active_test = 'active' if ((resp[region]['general']['active']) == 'true') else 'non-active'
-#     # pprint (active_test)
-#
-# # while True:
-# #     active_test = 'active'
-# #     if (resp[region]['general']['active']) == 'true':
-#         # break
-#
-# active_true = active_test.count('active')
-# active_false = active_test.count('non-active')
-# #
-# #
-# # COUNT OF ALL REGIONS
-# print('count total:')
-# print(len(data.keys()))
-# # print('')
-# print('-----')
-# #
-# #
-# # COUNT OF ACTIVE REGIONS
-# print('count active:')
-# print(active_true)
-# # print('')
-# print('-----')
-# #
-# #
-# # COUNT OF NON-ACTIVE REGIONS
-# print('count not active:')
-# print(active_false)
-# # print('')
-# print('-----')
-# print('-----')
-
-
-# active_true = active_test.count('true')
-# active_false = active_test.count('false')
--- parser5.py
+++ parser5.py
@@ -1,14 +1,12 @@
 import json
 import sys
-# from pprint import pprint
 
 with open('production.json') as file:
     data = json.load(file)
 
 json_str = json.dumps(data)
 resp = json.loads(json_str)
-#
-#
+
 # GET ALL REGION CODES AND ACTIVE STATUS
 print('region code, is active')
 print('-----')
@@ -17,42 +15,31 @@ for key in resp:
     status = (resp[region]['general']['active'])
     print (region, status)
 print('-----')
-# print('')
-#
-#
+
+
 # CLASSIFICATION OF REGIONS AS ACTIVE OR NON-ACTIVE
 for key in resp:
     region = key
     active_test = 'active' if ((resp[region]['general']['active']) == 'true') else 'non-active'
-    # pprint (active_test)
+    active_true = active_test.count('active')
+    active_false = active_test.count('non-active')
 
-# while True:
-#     active_test = 'active'
-#     if (resp[region]['general']['active']) == 'true':
-        # break
 
-active_true = active_test.count('active')
-active_false = active_test.count('non-active')
-#
-#
 # COUNT OF ALL REGIONS
 print('count total:')
 print(len(data.keys()))
-# print('')
 print('-----')
-#
-#
+
+
 # COUNT OF ACTIVE REGIONS
 print('count active:')
 print(active_true)
-# print('')
 print('-----')
-#
-#
+
+
 # COUNT OF NON-ACTIVE REGIONS
 print('count not active:')
 print(active_false)
-# print('')
 print('-----')
 print('-----')
 
